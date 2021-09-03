5963d008ee4db5d48587879f9d59d4344d75fd5f
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-09-02 21:22:10 -0700

    Added student route which leads users to their enrolled courses after stripe successfull payment using the [id] gotten from stripe callback url

--- components/routes/StudentRoute.js
+++ components/routes/StudentRoute.js
@@ -2,9 +2,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { SyncOutlined } from "@ant-design/icons";
-import UserNav from "../nav/UserNav";
 
-const UserRoute = ({ children, showNav = true }) => {
+const StudentRoute = ({ children, showNav = true }) => {
   // state
   const [ok, setOk] = useState(false);
   // router
@@ -34,15 +33,10 @@ const UserRoute = ({ children, showNav = true }) => {
           className='d-flex justify-content-center display-1 text-primary p-5'
         />
       ) : (
-        <div className='container-fluid'>
-          <div className='row'>
-            <div className='col-md-2'>{showNav && <UserNav />}</div>
-            <div className='col-md-10'>{children}</div>
-          </div>
-        </div>
+        <div className='container-fluid'>{children}</div>
       )}
     </>
   );
 };
 
-export default UserRoute;
+export default StudentRoute;
